c16158e403a2048bd3a811b2ed17c7c89edb1bcc
Marcin Polak <mpolak@MacBook-Air-Marcin.local>
2021-11-19 19:33:17 +0100

    init

---
+++ src/main/kotlin/pl/newoncequiz/newoncequiz/NewonceQuizApplication.kt
@@ -0,0 +1,11 @@
+package pl.newoncequiz.newoncequiz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewonceQuizApplication
+
+fun main(args: Array<String>) {
+    runApplication<NewonceQuizApplication>(*args)
+}
---
+++ src/test/kotlin/pl/newoncequiz/newoncequiz/NewonceQuizApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.newoncequiz.newoncequiz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewonceQuizApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
