f43079ade214a75ddd7346eb86fdcbaad79b6d51
mpolak420 <marcinpolak@live.com>
2021-11-19 23:04:57 +0100

    add cors headers

---
+++ src/main/kotlin/pl/newoncequiz/newoncequiz/configuration/CorsConfiguration.kt
@@ -0,0 +1,24 @@
+package pl.newoncequiz.newoncequiz.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedOrigins("*")
+                    .allowedHeaders("*")
+            }
+        }
+    }
+}
